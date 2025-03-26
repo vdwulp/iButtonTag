050d87b26f7007ea33ac9c9d72f26ffd352154fa
vdwulp <58190301+vdwulp@users.noreply.github.com>
2025-03-26 14:54:10 +0100

    Create Simple.ino

---
+++ examples/Simple/Simple.ino
@@ -0,0 +1,22 @@
+#include <iButtonTag.h>                     // Include the library
+
+#define PIN_PROBE 2                         // Data line connected to pin 2
+
+iButtonTag ibutton( PIN_PROBE );            // Setup iButtonTag on the pin
+
+void setup(void) {
+  Serial.begin(9600);
+}
+
+void loop(void) {
+  iButtonCode code;                         // Variable to store ID-code
+  
+  Serial.println( "Reading... " );
+  int8_t status = ibutton.readCode( code ); // Try to read ID-code
+  
+  if ( status > 0 ) {                       // Code read succesfully
+    Serial.print( "iButton code read: " );
+    ibutton.printCode( code );              // ID-code is in variable /code/
+    Serial.println();
+  }
+}
